306e69e79c1965ceb2ad9f8caa0374a1c3c89b33
daocers <daocers@gmail.com>
2017-04-17 23:59:04 +0800

    集合类测试。迭代中删除元素

--- tes/src/main/java/co/bugu/tes/controller/TradeController.java
+++ tes/src/main/java/co/bugu/tes/controller/TradeController.java
@@ -15,7 +15,23 @@ public class TradeController {
 
     private static Logger logger = LoggerFactory.getLogger(TradeController.class);
 
+    /**
+     * 录制交易
+     * 打开交易界面， 同时录入信息，记录当前交易界面的栏位信息，顺便出题一道
+     * @return
+     */
+    @RequestMapping("/toRecord")
     public String record(){
         return "trade/record";
+    }
+
+    @RequestMapping("/save")
+    public String saveModelAndQuestion(){
+        return null;
+    }
+
+    @RequestMapping("/saveQuestion")
+    public String saveQuestion(){
+        return null;
     }
 }
